8d85e61f73ea0eee3179dfd28dd0034325c42182
shakhawath <m.shakhawath18@gmail.com>
2022-02-27 23:16:52 +0600

    photo detail part done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadPhotos = async () => {
-    const url = ('https://jsonplaceholder.typicode.com/photos');
+    const url = 'https://jsonplaceholder.typicode.com/photos';
     const res = await fetch(url);
     const data = await res.json();
     displayPhotos(data);
@@ -8,16 +8,17 @@ const displayPhotos = photos => {
     // for (const photo of photos) {
     //     console.log(photo)
     // }
-    if (photos.length === 100) {
-        console.log(photos);
-    }
+
+    // console.log(photos);
+
     const album = document.getElementById('album');
     photos.forEach(photo => {
+        // console.log(photo)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
                 <div>
-                    <div onclick="showDetail('${photo}')" class="card h-100">
+                    <div onclick="loadDetail('${photo.id}')" class="card h-100">
                         <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${photo.title}</h5>
@@ -30,20 +31,27 @@ const displayPhotos = photos => {
 
     });
 };
-const showDetail = (photo) => {
+const loadDetail = async id => {
+    const url = `https://jsonplaceholder.typicode.com/photos/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showDetail(data);
+}
+const showDetail = photo => {
+    console.log(photo);
     const photodetail = document.getElementById('photo-detail');
     photodetail.innerHTML = '';
     const divs = document.createElement('div');
     divs.classList.add('card');
     divs.innerHTML = `                
-    <div class="h-100">
-    <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${photo.title}</h5>
-    <p class="card-text">${photo.id}</p>
-    <p class="card-text">${photo.url}</p>
-    </div>
-    </div>
+            
+                <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${photo.title}</h5>
+                    <p class="card-text">${photo.id}</p>
+                    <p class="card-text">${photo.url}</p>
+                </div>
+            
     `;
     photodetail.appendChild(divs);
 }
